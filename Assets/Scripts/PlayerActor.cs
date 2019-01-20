53304f6aafdd8711442e1afae3d23ce69312ce60
VIDDESLAPTOP\Vidar <vidar.halmstad@hotmail.com>
2019-01-20 19:27:49 +0100

    Changed temp names (and name position)

--- Assets/Scripts/PlayerActor.cs
+++ Assets/Scripts/PlayerActor.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerActor : PongActor {
+public class PlayerActor : PongActor
+{
 
-	public Paddle.Direction MakeDecision(Arena.State state) {
-		return Input.GetKey(KeyCode.A) ? Paddle.Direction.Left :
-		      (Input.GetKey(KeyCode.D) ? Paddle.Direction.Right : Paddle.Direction.None);
-	}
-	
-	public void OnGameStart(Arena.State state) {}	
-	public void OnGameEnd(Arena.State state) {}
+    public Paddle.Direction MakeDecision(Arena.State state)
+    {
+        return Input.GetKey(KeyCode.A) ? Paddle.Direction.Left :
+              (Input.GetKey(KeyCode.D) ? Paddle.Direction.Right : Paddle.Direction.None);
+    }
+
+    public void OnGameStart(Arena.State state) { }
+    public void OnGameEnd(Arena.State state) { }
 }

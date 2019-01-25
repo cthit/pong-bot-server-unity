0691bdf60453d6df09e797811bbb4611f0b6a62c
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2019-01-25 21:23:54 +0100

    Majestic smurf

---
+++ Assets/Scripts/SmurfAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmurfAnimation : MonoBehaviour {
+
+    private Vector2 basePosition;
+
+    private void Start() {
+        basePosition = transform.position;
+    }
+
+    private void Update() {
+        transform.position = basePosition + Vector2.down * 0.20f * Mathf.Sin(2.0f * Time.time);
+    }
+}
